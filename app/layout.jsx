271587f047a12dba5db9324c1f6c141d3190f2e8
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-02-25 16:17:53 +0100

    QueryWrapper has to wrap all the app

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Nav from './auth/Nav'
 import { Roboto } from "@next/font/google"
+import QueryWrapper from "./auth/QueryWrapper"
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -16,9 +17,11 @@ export default function RootLayout({children }) {
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className={`mx-4 md:mx-48 xl:mx-96 ${roboto.variable} bg-gray-200`}>
-        <Nav />
-        {children}
+      <body className={`mx-16 md:mx-36 xl:mx-96 ${roboto.variable} font-sans bg-gray-200`}>
+        <QueryWrapper>
+          <Nav />
+          {children}
+        </QueryWrapper>
       </body>
     </html>
   )

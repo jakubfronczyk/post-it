059227264ef3b54b72ba14b9dfd8e3a0fb4592e0
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-03-03 18:45:21 +0100

    Need to fix all types

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,28 +1,30 @@
-import './globals.css'
-import Nav from './auth/Nav'
-import { Roboto } from "@next/font/google"
-import QueryWrapper from "./auth/QueryWrapper"
+import "./globals.css";
+import Nav from "./auth/Nav";
+import { Roboto } from "@next/font/google";
+import QueryWrapper from "./auth/QueryWrapper";
 
 const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-roboto",
-})
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    variable: "--font-roboto",
+});
 
-export default function RootLayout({children }) {
-  return (
-    <html lang="en">
-      {/*
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body className={`mx-16 md:mx-36 xl:mx-96 ${roboto.variable} font-sans bg-gray-200`}>
-        <QueryWrapper>
-          <Nav />
-          {children}
-        </QueryWrapper>
-      </body>
-    </html>
-  )
+            <head />
+            <body
+                className={`mx-16 md:mx-36 xl:mx-96 ${roboto.variable} font-sans bg-gray-200`}
+            >
+                <QueryWrapper>
+                    <Nav />
+                    {children}
+                </QueryWrapper>
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,38 @@
-'use client'
+"use client";
 
-import AddPost from "./components/AddPost"
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import AddPost from "./components/AddPost";
+import Post from "./components/Post";
+import { PostsType } from "./types/Posts";
 
-
+//Fetch all posts
+const allPost = async () => {
+    const response = await axios.get("api/posts/getPosts");
+    return response.data;
+};
 
 export default function Home() {
-  return (
-    <main className="">
-      <AddPost />
-    </main>
-  )
+    const { data, error, isLoading } = useQuery<PostsType>({
+        queryFn: allPost,
+        queryKey: ["posts"],
+    });
+    if (error) return error;
+    if (isLoading) return "Loading...";
+
+    return (
+        <main className="">
+            <AddPost />
+            {data?.map((post: PostsType) => (
+                <Post
+                    key={post.id}
+                    id={post.id}
+                    name={post.user.name}
+                    avatar={post.user.image}
+                    title={post.title}
+                    comments={post.comments}
+                />
+            ))}
+        </main>
+    );
 }
